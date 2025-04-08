b97c038805510ebf5950d31f9333caa28f95aefb
aboodtouq <aboodtamer60@gmail.com>
2025-04-08 22:13:28 +0300

    add session data fields to the model

--- src/main/java/com/spring/techpractica/model/Sessions.java
+++ src/main/java/com/spring/techpractica/model/Sessions.java
@@ -1,8 +1,31 @@
 package com.spring.techpractica.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "session")
 public class Sessions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int sessionId;
     private String sessionName;
     private String sessionDescription;
     private boolean isPrivate;
+    @OneToMany
+    private List<User> users= new ArrayList<>();
+
+    public Sessions() {
+
+    }
+
+    public Sessions(int sessionId, String sessionName, String sessionDescription, boolean isPrivate) {
+        this.sessionId = sessionId;
+        this.sessionName = sessionName;
+        this.sessionDescription = sessionDescription;
+        this.isPrivate = isPrivate;
+    }
+
 }
