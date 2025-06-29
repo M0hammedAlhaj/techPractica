f8a6725c85fd7d91e695ab2c4bcd6d5200acfecf
3raffat <mhmd3raffat@gmail.com>
2025-06-29 19:12:37 +0300

    simple edits

--- frontend/src/data/data.ts
+++ frontend/src/data/data.ts
@@ -117,3 +117,34 @@ export const skills = [
   "TypeScript",
   "Node.js",
 ];
+
+interface Inav {
+  path: string;
+  label: string;
+}
+export const NavLinks: Inav[] = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Learn",
+    path: "/Learn",
+  },
+  {
+    label: "Sessions",
+    path: "/Sessions",
+  },
+  {
+    label: "Profile",
+    path: "/Profile",
+  },
+  {
+    label: "Login",
+    path: "/User",
+  },
+  {
+    label: "Join",
+    path: "User/Register",
+  },
+];
