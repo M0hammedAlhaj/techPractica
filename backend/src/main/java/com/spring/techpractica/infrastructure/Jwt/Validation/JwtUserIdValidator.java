71b082ad256651d8b73e6e993afc4d80dcfb97dc
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-28 22:07:04 +0300

    fix(backend):user id validation compare with database

--- backend/src/main/java/com/spring/techpractica/infrastructure/Jwt/Validation/JwtUserIdValidator.java
+++ backend/src/main/java/com/spring/techpractica/infrastructure/Jwt/Validation/JwtUserIdValidator.java
@@ -1,11 +1,14 @@
 package com.spring.techpractica.infrastructure.Jwt.Validation;
 
+import com.spring.techpractica.Core.User.User;
+import com.spring.techpractica.Core.User.UserRepository;
 import com.spring.techpractica.infrastructure.Cor.AbstractHandler;
 import com.spring.techpractica.infrastructure.Jwt.Exception.JwtValidationException;
 import com.spring.techpractica.infrastructure.Jwt.JwtExtracting;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -13,13 +16,15 @@ import java.util.UUID;
 public class JwtUserIdValidator extends AbstractHandler<JwtValidationContext> {
 
     private final JwtExtracting jwtExtracting;
+    private final UserRepository userRepository;
 
     @Override
     public void process(JwtValidationContext context) {
         UUID actualId = jwtExtracting.extractId(context.token());
+        Optional<User> user = userRepository.findById(actualId);
 
-        if (!actualId.equals(context.expectedUserId())) {
-            throw new JwtValidationException("User ID in token does not match the request");
+        if (user.isEmpty()) {
+            throw new JwtValidationException("User ID in token does not match the database");
         }
     }
 }
