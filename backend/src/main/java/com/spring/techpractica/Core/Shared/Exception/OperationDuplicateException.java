6e3be7e04b373d5107685fb6f98082e048ad961d
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-29 02:30:06 +0300

    refact(backend):change message exception

--- backend/src/main/java/com/spring/techpractica/Core/Shared/Exception/OperationDuplicateException.java
+++ backend/src/main/java/com/spring/techpractica/Core/Shared/Exception/OperationDuplicateException.java
@@ -3,7 +3,7 @@ package com.spring.techpractica.Core.Shared.Exception;
 public class OperationDuplicateException extends RuntimeException {
 
     public OperationDuplicateException(String operationName) {
-        super(String.format("Operation already exists or was already executed: %s", operationName));
+        super(String.format("Operation was already executed: %s", operationName));
     }
 
 }
