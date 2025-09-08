dceafb8ab85e8dc6751a5ebf28fad6a6a3628450
qandeel <mohaqan2004@gmail.com>
2025-09-08 19:39:21 +0300

    use AddRequirementsForSessionService in UpdateSessionUseCase

--- backend/src/main/java/com/spring/techpractica/Application/Session/update/UpdateSessionUseCase.java
+++ backend/src/main/java/com/spring/techpractica/Application/Session/update/UpdateSessionUseCase.java
@@ -1,5 +1,6 @@
 package com.spring.techpractica.Application.Session.update;
 
+import com.spring.techpractica.Application.Session.create.CreateSessionCommand;
 import com.spring.techpractica.Core.Field.Entity.Field;
 import com.spring.techpractica.Core.Field.FieldRepository;
 import com.spring.techpractica.Core.Requirement.Entity.Requirement;
@@ -7,6 +8,7 @@ import com.spring.techpractica.Core.Requirement.RequirementFactory;
 import com.spring.techpractica.Core.RequirementTechnology.RequirementTechnologyFactory;
 import com.spring.techpractica.Core.Session.Entity.Session;
 import com.spring.techpractica.Core.Session.SessionRepository;
+import com.spring.techpractica.Core.Session.service.AddRequirementsForSessionService;
 import com.spring.techpractica.Core.SessionMembers.model.Role;
 import com.spring.techpractica.Core.Shared.Exception.ResourcesNotFoundException;
 import com.spring.techpractica.Core.Shared.Exception.UnauthorizedActionException;
@@ -31,10 +33,7 @@ public class UpdateSessionUseCase {
     private final SessionRepository sessionRepository;
     private final UserRepository userRepository;
     private final SystemRepository systemRepository;
-    private final FieldRepository fieldRepository;
-    private final RequirementFactory requirementFactory;
-    private final TechnologyRepository technologyRepository;
-    private final RequirementTechnologyFactory requirementTechnologyFactory;
+    private final AddRequirementsForSessionService requirementsForSessionService;
 
     @Transactional
     public Session execute(UpdateSessionCommand command) {
@@ -68,23 +67,13 @@ public class UpdateSessionUseCase {
 
         session.clearRequirements();
 
-        for (var requirementRequest : command.requirements()) {
-            Field field = fieldRepository.getOrThrowByID(requirementRequest.getFieldId());
-
-            Requirement requirement = requirementFactory.create(session, field);
-            session.addRequirement(requirement);
-
-            List<Technology> technologies = technologyRepository
-                    .findAllByIds(new HashSet<>(requirementRequest.getTechnologies()));
-
-            if (technologies.size() != requirementRequest.getTechnologies().size()) {
-                throw new ResourcesNotFoundException(requirementRequest.getTechnologies().toString());
-            }
-
-            technologies.stream()
-                    .map(tech -> requirementTechnologyFactory.create(requirement, tech))
-                    .forEach(requirement::addRequirementTechnology);
-
-        }
+        requirementsForSessionService.addRequirementsForSession(session, new CreateSessionCommand(
+                command.userId(),
+                command.name(),
+                command.description(),
+                command.isPrivate(),
+                command.system(),
+                command.requirements()
+        ));
     }
 }
