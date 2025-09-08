3323ae4e6c3c96bfe7a47287261773f25de6f4bb
qandeel <mohaqan2004@gmail.com>
2025-09-08 19:52:27 +0300

    refact AddRequirementsForSessionService

--- backend/src/main/java/com/spring/techpractica/Core/Session/service/AddRequirementsForSessionService.java
+++ backend/src/main/java/com/spring/techpractica/Core/Session/service/AddRequirementsForSessionService.java
@@ -7,19 +7,15 @@ import com.spring.techpractica.Core.Requirement.Entity.Requirement;
 import com.spring.techpractica.Core.Requirement.RequirementFactory;
 import com.spring.techpractica.Core.RequirementTechnology.RequirementTechnologyFactory;
 import com.spring.techpractica.Core.Session.Entity.Session;
-import com.spring.techpractica.Core.Session.SessionFactory;
-import com.spring.techpractica.Core.Session.SessionRepository;
-import com.spring.techpractica.Core.SessionMembers.SessionMembersFactory;
 import com.spring.techpractica.Core.Shared.Exception.ResourcesNotFoundException;
-import com.spring.techpractica.Core.System.SystemRepository;
 import com.spring.techpractica.Core.Technology.Entity.Technology;
 import com.spring.techpractica.Core.Technology.TechnologyRepository;
-import com.spring.techpractica.Core.User.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -31,22 +27,31 @@ public class AddRequirementsForSessionService {
     private final RequirementTechnologyFactory requirementTechnologyFactory;
 
     public void addRequirementsForSession(Session session, CreateSessionCommand command) {
-        for (var requirementRequest : command.requirements()) {
-            Field field = fieldRepository.getOrThrowByID(requirementRequest.getFieldId());
-
-            Requirement requirement = requirementFactory.create(session, field);
+        command.requirements().forEach(requirementRequest -> {
+            Requirement requirement = createRequirement(session, requirementRequest.getFieldId());
             session.addRequirement(requirement);
 
-            List<Technology> technologies = technologyRepository
-                    .findAllByIds(new HashSet<>(requirementRequest.getTechnologies()));
+            List<Technology> technologies = fetchTechnologies(requirementRequest.getTechnologies());
+            addTechnologiesToRequirement(requirement, technologies);
+        });
+    }
 
-            if (technologies.size() != requirementRequest.getTechnologies().size()) {
-                throw new ResourcesNotFoundException(requirementRequest.getTechnologies().toString());
-            }
+    private Requirement createRequirement(Session session, UUID fieldId) {
+        Field field = fieldRepository.getOrThrowByID(fieldId);
+        return requirementFactory.create(session, field);
+    }
 
-            technologies.stream()
-                    .map(tech -> requirementTechnologyFactory.create(requirement, tech))
-                    .forEach(requirement::addRequirementTechnology);
+    private List<Technology> fetchTechnologies(List<UUID> technologyIds) {
+        List<Technology> technologies = technologyRepository.findAllByIds(new HashSet<>(technologyIds));
+        if (technologies.size() != technologyIds.size()) {
+            throw new ResourcesNotFoundException(technologyIds.toString());
         }
+        return technologies;
+    }
+
+    private void addTechnologiesToRequirement(Requirement requirement, List<Technology> technologies) {
+        technologies.stream()
+                .map(tech -> requirementTechnologyFactory.create(requirement, tech))
+                .forEach(requirement::addRequirementTechnology);
     }
 }
